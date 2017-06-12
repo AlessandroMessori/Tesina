d39ca5e7cb5c4ec9ca7216bfa65f842ef898975d
AlessandroMessori <allemex@libero.it>
2017-06-12 12:17:45 +0200

    added console text

--- src/client/components/Pages/Origin/index.js
+++ src/client/components/Pages/Origin/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {Jumbotron, PageHeader} from 'react-bootstrap'
 import NavBar from '../../Widgets/NavBar'
+import ConsoleText from '../../Widgets/ConsoleText'
 import ScrollBtn from '../../Widgets/ScrollBtn'
 import './index.scss'
 
 const Origin = (props) => (<div className='origin-section container-fluid'>
   <NavBar black/>
   <Jumbotron>
-    <PageHeader>utopia</PageHeader>
+    <ConsoleText id="text1">utopia</ConsoleText>
     <h2 id="quote"> Una mappa del mondo che non include Utopia non è degna nemmeno di uno sguardo, perché non contempla
       il solo paese al quale l’umanità approda di continuo. E quando vi approda, l’umanità si guarda intorno, vede un
       paese migliore e issa nuovamente le vele.</h2>
@@ -15,7 +16,7 @@ const Origin = (props) => (<div className='origin-section container-fluid'>
     <ScrollBtn id='scrollBtn1' target='#moore'/>
   </Jumbotron>
   <Jumbotron id='moore' className='container-fluid'>
-    <PageHeader>Origine Del Termine</PageHeader>
+    <ConsoleText id="text2">Origine Del Termine</ConsoleText>
     <h2>L'Utopìa è un romanzo di Tommaso Moro pubblicato nel 1516, in cui è descritto il viaggio immaginario di Raffaele
       Itlodeo in una fittizia isola-regno, abitata da una società ideale.</h2>
     <img className="col-md-6" src="images/moore.jpeg" role='presentation'/>
@@ -23,7 +24,7 @@ const Origin = (props) => (<div className='origin-section container-fluid'>
     <ScrollBtn id='scrollBtn3' target='#distopia'/>
   </Jumbotron>
   <Jumbotron id='distopia' className='container-fluid'>
-    <PageHeader>La Nascita della Distopia</PageHeader>
+    <ConsoleText id="text3">La Nascita della Distopia</ConsoleText>
     <h2>Per distopia s'intende la descrizione di una immaginaria società o comunità altamente indesiderabile o
       spaventosa. Il termine è stato coniato come contrario di utopia ed è soprattutto utilizzato in riferimento alla
       rappresentazione di una società fittizia (spesso ambientata nel futuro) nella quale alcune tendenze sociali,
