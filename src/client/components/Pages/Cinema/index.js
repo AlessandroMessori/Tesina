2a73c9869a91f6e33bcbb5528a6bbeb18019ff90
AlessandroMessori <allemex@libero.it>
2017-06-28 18:15:16 +0200

    update

--- src/client/components/Pages/Cinema/index.js
+++ src/client/components/Pages/Cinema/index.js
@@ -8,24 +8,17 @@ import './index.scss'
 const Cinema = (props) => (<div className='cinema-section container-fluid'>
   <NavBar black/>
   <Jumbotron className="content-wrapper">
-    <ConsoleText id="text1">The Terminator</ConsoleText>
-    <hr/>
-    <img className="col-md-6" src="images/android.jpeg" role='presentation'/>
-    <img className="col-md-6" src="images/terminator.jpeg" role='presentation'/>
-    <ScrollBtn id='scrollBtn1' target='#matrix'/>
-  </Jumbotron>
-  <Jumbotron id='matrix' className='container-fluid content-wrapper'>
-    <ConsoleText id="text2">The Matrix</ConsoleText>
-    <hr/>
-    <img className="col-md-6" src="images/numbers.jpeg" role='presentation'/>
-    <img className="col-md-6" src="images/matrix.jpeg" role='presentation'/>
-    <ScrollBtn id='scrollBtn2' target='#inTime'/>
-  </Jumbotron>
-  <Jumbotron id='inTime' className='container-fluid content-wrapper'>
     <ConsoleText id="text3">In Time</ConsoleText>
     <hr/>
     <img className="col-md-6" src="images/intime.jpeg" role='presentation'/>
     <img className="col-md-6" src="images/clock.jpg" role='presentation'/>
+    <ScrollBtn id='scrollBtn1' target='#matrix'/>
+  </Jumbotron>
+  <Jumbotron id='matrix' className='container-fluid content-wrapper'>
+    <ConsoleText id="text2">Nerve</ConsoleText>
+    <hr/>
+    <img className="col-md-6" src="images/nerve.jpeg" role='presentation'/>
+    <img className="col-md-6" src="images/dare.jpg" role='presentation'/>
   </Jumbotron>
 </div>)
 
